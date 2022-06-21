c5d68477c776eca1dddedaadf0676bbab3db9a4f
Lorenz Metzner <lorenz-michael-simon.metzner@stud.uni-bamberg.de>
2022-06-21 12:21:46 +0200

    added error handling for existing account

--- app/frontend/models/RegisterModel.php
+++ app/frontend/models/RegisterModel.php
@@ -34,6 +34,12 @@ class RegisterModel extends Model {
      * @return bool based on execution success
      */
     public function register() {
+        $statement1 = $db->prepare("SELECT * FROM Users WHERE email = $this->email");
+        while ($row = $statement1->fetch()) {
+            if (!empty($row['email'])) {
+                exit;                           // TO DO complete error handling
+            }
+        }
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         $statement = $this->db->prepare("INSERT INTO User (firstname, lastname, email, password, gender, birthdate) VALUES (:firstname, :lastname, :email, :password, :gender, :birthdate)");
         $statement->bindValue(':firstname', $this->firstname);
