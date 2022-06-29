89f7e4ccc38fa43a46ba41997353a5b727836fe1
Henrik Lammert <henrik.lammert@stud.uni-bamberg.de>
2022-06-29 11:25:32 +0200

    Fixes: Case Sensitivity in some require_once() statements. App now runs on webserver

--- app/frontend/models/PWResetEmailModel.php
+++ app/frontend/models/PWResetEmailModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'model.php';
+require_once 'Model.php';
 require_once 'Database.php';
 
 class PWResetEmailModel extends model {
--- app/frontend/models/VisitenkartenModel.php
+++ app/frontend/models/VisitenkartenModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'model.php';
+require_once 'Model.php';
 require_once 'app/frontend/models/HasInterestModel.php';
 
 class VisitenkartenModel extends Model{
