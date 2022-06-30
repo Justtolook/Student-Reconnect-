cbdadd18589a58ea0f203303942cdd0b9feb65c5
David C <david.cloos@stud.uni-bamberg.de>
2022-06-30 14:24:19 +0200

    add ShowProfileModel

--- app/frontend/controllers/ProfileController.php
+++ app/frontend/controllers/ProfileController.php
@@ -2,17 +2,20 @@
 require_once 'Controller.php';
 require_once 'app/frontend/models/VisitenkartenModel.php';
 require_once 'app/frontend/models/InterestModel.php';
+require_once 'app/frontend/models/ShowProfileModel.php';
 
 require_once 'Database.php';
 
 class ProfileController extends Controller {
     public VisitenkartenModel $visitenkartenModel;
     public InterestModel $interestModel;
+    public ShowProfileModel $ShowProfileModel;
     public int $id_user;
 
     public function __construct() {
         $this->id_user = $_SESSION['user']['id_user'];
         $this->visitenkartenModel = new VisitenkartenModel($this->id_user);
+        $this->ShowProfileModel = new ShowProfileModel($this->id_user);
         $this->interestModel = new InterestModel();
     }
     /**
@@ -21,7 +24,7 @@ class ProfileController extends Controller {
      */
     public function profile() {
         $this->interestModel->fetchAllInterests();
-        return $this->render("profile", ["visitenkarte" => $this->visitenkartenModel, "interestModel" => $this->interestModel]);
+        return $this->render("profile", ["visitenkarte" => $this->visitenkartenModel, "interestModel" => $this->interestModel, "profile" => $this->ShowProfileModel]);
     }
 
 }
--- app/frontend/views/profile.php
+++ app/frontend/views/profile.php
@@ -5,4 +5,25 @@
 <?php
 // print own visitenkarte
 include 'app/frontend/views/visitenkartenModal.php';
+
+
+//Show Profile Information 
+if($profile->id_user != $_SESSION['user']['id_user']) {
+    echo '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ProfileTestModal">
+        User melden
+    </button>';
+}
+echo "Name: " . $profile->firstname . " " . $profile->lastname . "<br>";
+echo "Geschlecht: " . $profile->gender . "<br>";
+echo "scoreHost: " . $profile->scoreHost . "<br>";
+echo "scoreAttendee: " . $profile->scoreAttendee . "<br>";
+echo "Beschreibung: " . $profile->description . "<br>";
+echo "Interessen: <br>";
+echo "<ul>";
+foreach ($profile->interests as $interest) {
+    echo "<li>" . $interestModel->getInterestName($interest) . "</li>";
+}
+echo "</ul>";
+echo "Kontaktinformationen: <br>" . $profile->contactInformation;
+
 ?>
