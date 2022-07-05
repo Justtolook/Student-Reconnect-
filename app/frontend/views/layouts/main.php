82d4f6b6997583f6cecc6fd753990bc05d970fac
Kai Leon Marquardt <kai-leon.marquardt@stud.uni-bamberg.de>
2022-07-05 02:07:55 +0200

    matching view basics styled

--- app/frontend/views/layouts/main.php
+++ app/frontend/views/layouts/main.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="app/frontend/css/bootstrap.css">
     <link rel="stylesheet" href="app/frontend/css/colours.css">
     <link rel="stylesheet" href="app/frontend/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <!-- define navbar -->
--- app/frontend/views/matching.php
+++ app/frontend/views/matching.php
@@ -1,131 +1,74 @@
-<h1>Matching</h1>
 <div class="matchCard">
     <div class="text-center">
-        <button class="btn text-center" onclick="showFilter()">Filter</button>
+        <button class="btn text-center" onclick="showFilter()">Filter deiner Suche</button>
     </div>
     <div class="matchingFilter">
-        <div class="row">
-            <div class="col-md-12">
-                <label class="PillList-item">
-                    <input type="checkbox" name="feature" value="1">
-                    <span class="PillList-label">one
-                    <span class="Icon Icon--checkLight Icon--smallest">
-                        </span>
-                        </span>
+        <div class="row d-flex justify-content-center">
+            <span class="mb-3 mt-2">Interessen</span>
+            <div class="col-md-11">
                 </label>
                 <!-- checkboxes with interest to filter the user list -->
                 <form action="?t=frontend&request=matching/filter" method="post">
                     <?php
                     foreach ($interestModel->interests as $interest) {
-                        echo '<lable class="PillList-item">' . '<input type="checkbox" name="interests[]" value="' .
-                            $interest . '">' . '<span class="PillList-label">' . $interest . '<span class="Icon Icon--checkLight Icon--smallest"><i class="fa fa-check"></i></span>' .
-                            '</span>' . '</lable>';
+                        echo '<label class="PillList-item"><input type="checkbox" name="interests[]" value="' .
+                            $interest . '"><span class="PillList-label">' . $interest .
+                            '<span class="Icon Icon--checkLight Icon--smallest"><i class="fa fa-check"></i></span></span></label>';
                     }
                     ?>
-                    <br>
+                    <br class="spacer">
                     <input class="btn" type="submit" name="filter" value="Filter setzen">
+                </form>
+                <!-- reset filter button -->
+                <form action="?t=frontend&request=matching/resetfilter" method="post">
+                    <input class="btn float-left" type="submit" name="reset" value="Filter zurücksetzen">
+                </form>
+                <!-- clear filter button -->
+                <form action="?t=frontend&request=matching/clearfilter" method="post">
+                    <input class="btn" type="submit" name="clear" value="Filter löschen">
                 </form>
             </div>
         </div>
-        <!-- reset filter button -->
-        <form action="?t=frontend&request=matching/resetfilter" method="post">
-            <input class="btn" type="submit" name="reset" value="Reset Filter">
-        </form>
-        <!-- clear filter button -->
-        <form action="?t=frontend&request=matching/clearfilter" method="post">
-            <input class="btn" type="submit" name="clear" value="Clear Filter">
-        </form>
     </div>
-    <div>
-        <h2>
-            <?php echo $model->firstname . " " . $model->lastname; ?>
-        </h2>
-        image
-        <?php echo $model->description; ?>
-
-        Interessen: <br>
-        <!-- print the interests of the user in plain text-->
-        <span>
-                        <?php foreach ($model->interests as $interest) {
-                            echo "<li>" . $interestModel->getInterestName($interest) . "</li>";
-                        } ?>
-                    </span>
-
-        <!-- two buttons: one for matching and one for not matching -->
-        <form action="?t=frontend&request=matching/matching" method="post">
-            <input type="hidden" name="id_user" value="<?php echo $model->id_user; ?>">
-            <input type="submit" value="Matchen">
-        </form>
-        <form action="?t=frontend&request=matching/notmatching" method="post">
-            <input type="hidden" name="id_user" value="<?php echo $model->id_user; ?>">
-            <input type="submit" value="Nicht Matchen">
-        </form>
+    <div class="card pastelgrün">
+        <div class="card-header">
+            <h2>
+                <?php echo $model->firstname . " " . $model->lastname; ?>
+            </h2>
+            <div class="text-center">
+                <img class="card-img w-50 mt-2 mb-4" src="res/Marie Becker.png" alt="Profilbild">
+            </div>
+        </div>
+        <div class="card-body">
+            <p>
+                <?php echo $model->description; ?>
+            </p>
+            <p>Interessen:
+                <!-- print the interests of the user in plain text-->
+                <?php foreach ($model->interests as $interest) {
+                    echo "<li>" . $interestModel->getInterestName($interest) . "</li>";
+                } ?>
+            </p>
+        </div>
+        <div class="card-footer row justify-content-md-center">
+            <div class="col col-lg-2 text-center">
+                <!-- two buttons: one for matching and one for not matching -->
+                <form action="?t=frontend&request=matching/matching" method="post">
+                    <input type="hidden" name="id_user" value="<?php echo $model->id_user; ?>">
+                    <button class="bg-white" type="submit" id="matchen">
+                        <i class="fa-solid fa-check text-success"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="col col-lg-2 text-center">
+                <form action="?t=frontend&request=matching/notmatching" method="post">
+                    <input type="hidden" name="id_user" value="<?php echo $model->id_user; ?>">
+                    <button class="bg-white" type="submit" id="nichtMatchen">
+                        <i class="fa-solid fa-xmark text-danger bg-white"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 <script src="app/frontend/js/matching.js"></script>
-
-<style>
-    .PillList-item {
-        cursor: pointer;
-        display: inline-block;
-        float: left;
-        font-size: 14px;
-        font-weight: normal;
-        line-height: 20px;
-        margin: 0 12px 12px 0;
-        text-transform: capitalize;
-    }
-
-    .PillList-item input[type="checkbox"] {
-        display: none;
-    }
-
-    .PillList-item input[type="checkbox"]:checked + .PillList-label {
-        background-color: #40bf8e;
-        border: 1px solid rgb(255, 255, 255);
-        color: rgb(255, 255, 255);
-        padding-right: 16px;
-        padding-left: 16px;
-    }
-
-    .PillList-label {
-        border: 1px solid #40bf8e;
-        border-radius: 20px;
-        color: #40bf8e;
-        display: block;
-        padding: 7px 28px;
-        text-decoration: none;
-    }
-
-    .PillList-item
-    input[type="checkbox"]:checked
-    + .PillList-label
-    .Icon--checkLight {
-        display: inline-block;
-    }
-
-    .PillList-item input[type="checkbox"]:checked + .PillList-label .Icon--addLight,
-    .PillList-label .Icon--checkLight,
-    .PillList-children {
-        display: none;
-    }
-
-    .PillList-label .Icon {
-        width: 12px;
-        height: 12px;
-        margin: 0 0 0 12px;
-    }
-
-    .Icon--smallest {
-        width: 12px;
-        height: 12px;
-    }
-
-    .Icon {
-        background: transparent;
-        display: inline-block;
-        font-style: normal;
-        vertical-align: baseline;
-        position: relative;
-    }
-</style>
