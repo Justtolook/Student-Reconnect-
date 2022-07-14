6d56096808b944d72fc7f1e3e25beda803e73612
Anastasiya <anastasiya.samarchuk@stud.uni-bamberg.de>
2022-07-14 17:01:36 +0200

    Profile styling #4

--- app/frontend/views/profile.php
+++ app/frontend/views/profile.php
@@ -4,30 +4,32 @@
 <br>
 <br>
 <div class="container-fluid">
-<div class="row text-center">
+    <div class="row text-center">
 
-    <!--Grid column-->
-    <div class="col-md-8 mb-4">
+        <!--Grid column-->
+        <div class="col-md-8 mb-4">
 
-        <img class="rounded-circle w-50 mt-2 mb-2" alt="Profile Image" src="<?php echo ($profilepicmodel->getProfileImagePath())?>"
-             data-holder-rendered="true">
+            <img class="rounded-circle w-50 mt-2 mb-2" alt="Profile Image"
+                 src="<?php echo($profilepicmodel->getProfileImagePath()) ?>"
+                 data-holder-rendered="true">
 
+        </div>
+        <div class="col-md-4 mb-4">
+
+            <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal">
+                Profilbild bearbeiten
+            </button>
+        </div>
     </div>
-    <div class="col-md-4 mb-4">
-
-    <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal">
-        Profilbild bearbeiten
-    </button>
-</div>
-</div>
 </div>
 
 
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalTitle" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalTitle"
+     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalTitle" >Profil</h5>
+                <h5 class="modal-title" id="exampleModalTitle">Profil</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -36,9 +38,6 @@
                 <?php
                 include 'profilepicedit.php';
                 ?>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Schließen</button>
             </div>
         </div>
     </div>
@@ -55,11 +54,11 @@
     <tr>
         <td scope="row">
             <div class="card">
-            <div class="card-body">
-                <?php
-                echo $profile->firstname;
-                ?>
-            </div>
+                <div class="card-body">
+                    <?php
+                    echo $profile->firstname;
+                    ?>
+                </div>
             </div>
         </td>
         <td>
@@ -123,17 +122,14 @@
 </table>
 
 
-
-
-
-
 <!-- Button trigger modal -->
 <button type="button" class="btn float-right" data-toggle="modal" data-target="#exampleModalCenter">
     Visitenkarte
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
