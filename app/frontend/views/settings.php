5903ebfb696a7fba63eb5f267fc84ef2890177e9
Riley Lew <riley-donald-lorenz.lew@stud.uni-bamberg.de>
2022-07-19 17:20:39 +0200

    Bug fix settings

--- app/frontend/views/settings.php
+++ app/frontend/views/settings.php
@@ -10,25 +10,29 @@
             </div>
         </div>
     </div>
-    <div style = "padding: 150px 100px 10px;" class="row justify-content-center">
         <br><h1>Das Impressum findest du hier:</h1>
-        <a href="?t=frontend&request=impressum" style = "padding: 50px 10px 10px;" class="link-primmary">Impressum</a><br>
-    </div>
-</div>
-<div style = "padding: 50px 100px 10px;" class="row justify-content-center">
-    <div class="form-group">
+        <div style = "padding: 50px 10px 10px;" class="row justify-content-center">
+            <a href="?t=frontend&request=impressum" class="link-primmary float-none">Impressum</a><br>
+        </div>
+    <div class="form-group row justify-content-center">
         <form action="?t=frontend&request=logout" method="post">
-            <input type="submit" value="Logout" class="btn text-center">
+            <div style = "padding: 50px 10px 10px;" class="row justify-content-center">
+                <button type="submit" class="btn text-center">Logout</button>
+            </div>
         </form>
     </div>
-</div>
+    <div class="form-group row justify-content-center">
 <?php
 if(Application::$app->controller->isModerator()) {
-    echo '<div style = "padding: 20px 100px 10px;" class="form-group">
+    echo '<div style = "padding: 20px 10px 10px;" class="form-group">
     <form action="?t=frontend&request=moderation" method="post">
         <button class="btn text-center">Moderationsbereich </button>
     </form>
     </div>';}
+?>
+    </div>
+    <div class="form-group row justify-content-center">
+        <?php
 if(Application::$app->controller->isAdmin()) {
     echo '<div class="form-group">
     <form action="?t=backend&request=user" method="post">
@@ -36,5 +40,7 @@ if(Application::$app->controller->isAdmin()) {
     </form>
     </div>';}
 ?>
+    </div>
+</div>
 </div>
 
