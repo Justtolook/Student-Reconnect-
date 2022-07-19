9e61c6c12b3904a584abb3d99097f8279cd2e8df
ba063966 <henrik.lammert@stud.uni-bamberg.de>
2022-07-19 10:15:00 +0000

    Revert "Merge branch 'feature/frontendLoginSystem' into 'main'"
    This reverts commit ce6a6821d752b7fa2d248c52cb96e7aa2e676409

--- app/frontend/views/login.php
+++ app/frontend/views/login.php
@@ -1,29 +1,21 @@
-<p class="container-fluid">
-    <div style = "padding: 100px 100px 10px;">
-        <br><h1>Bei Student Reconnect anmelden</h1><br><br>
-        <form method="POST" action="?t=frontend&request=login">
-            <div class="form-group row justify-content-center">
-                <input type="email" class="form-control w-75 border border-dark <?php echo $model->hasError('email') ? 'is-invalid' : ''?>" id="email" name="email" placeholder="E-Mail">
-                <div class="invalid-feedback">
-                    <?php echo $model->getError('email') ?>
-                </div>
-            </div> <br>
-            <div class="form-group row justify-content-center">
-                <input type="password" class="form-control w-75 border border-dark <?php echo $model->hasError('password') ? 'is-invalid' : ''?>" id="password" name="password" placeholder="Passwort">
-                <div class="invalid-feedback">
-                    <?php echo $model->getError('password') ?>
-                </div>
-            </div>
-    </div><br>
-    <a href="?t=frontend&request=pwreset" class="link-primary float-right">Passwort vergessen?</a><br>
-<div style = "padding: 100px 100px 10px;" class="row justify-content-center">
-    <button type="submit" class="btn btn-success mx-auto">Einloggen</button><br><br>
-</div>
-    <div style = "padding: 500px 100px 10px;">
-        <br><h2>Du hast noch keinen Account?</h2><br>
-        <div class="row justify-content-center">
-            <a href="?t=frontend&request=register" class="btn mx-auto">Jetzt registrieren</a><br>
+<div class="container-fluid">
+<br><h1>Bei Student Reconnect anmelden</h1><br><br>
+<form method="POST" action="?t=frontend&request=login">
+    <div class="form-group">
+        <input type="email" class="form-control <?php echo $model->hasError('email') ? 'is-invalid' : ''?>" id="email" name="email" placeholder="E-Mail">
+        <div class="invalid-feedback">
+            <?php echo $model->getError('email') ?>
         </div>
-    </div>
-    </form>
+    </div><br>
+    <div class="form-group">
+        <input type="password" class="form-control <?php echo $model->hasError('password') ? 'is-invalid' : ''?>" id="password" name="password" placeholder="Passwort">
+        <div class="invalid-feedback">
+            <?php echo $model->getError('password') ?>
+        </div>
+    </div><br>
+    <a href="?t=frontend&request=pwreset" class="pw-reset">Passwort vergessen?</a>
+    <button type="submit" class="btn btn-primary">Einloggen</button><br><br>
+    <h2>Du hast noch keinen Account?</h2><br>
+    <a href="?t=frontend&request=register" class="btn">Jetzt registrieren</a><br>
+</form>
 </div>
